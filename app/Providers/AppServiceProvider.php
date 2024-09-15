29236cb05bd050570b5331efd4981b700a2cbf78
syafiq <syafiq40a@gmail.com>
2024-09-15 14:56:07 +0700

    push

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,34 @@ namespace App\Providers;
 
 use App\Repositories\CarrierGroupRepository;
 use App\Repositories\Interfaces\CarrierGroupRepositoryInterface;
+use App\Repositories\CarrierRequirementRepository;
+use App\Repositories\Interfaces\CarrierRequirementRepositoryInterface;
+use App\Repositories\CarrierEducationRepository;
+use App\Repositories\Interfaces\CarrierEducationRepositoryInterface;
+use App\Repositories\CarrierIndustrialsectorRepository;
+use App\Repositories\Interfaces\CarrierIndustrialsectorRepositoryInterface;
+use App\Repositories\CarrierSpecialistRepository;
+use App\Repositories\Interfaces\CarrierSpecialistRepositoryInterface;
+use App\Repositories\CarrierSkillRepository;
+use App\Repositories\Interfaces\CarrierSkillRepositoryInterface;
+use App\Repositories\CarrierRepository;
+use App\Repositories\Interfaces\CarrierRepositoryInterface;
+use App\Repositories\CompanieRepository;
+use App\Repositories\Interfaces\CompanieRepositoryInterface;
+use App\Repositories\CarrierdetailspecialistRepository;
+use App\Repositories\Interfaces\CarrierdetailspecialistRepositoryInterface;
+use App\Repositories\RegencieRepository;
+use App\Repositories\Interfaces\RegencieRepositoryInterface;
+use App\Repositories\ProvinceRepository;
+use App\Repositories\Interfaces\ProvinceRepositoryInterface;
+use App\Repositories\CarrierdetailskillRepository;
+use App\Repositories\Interfaces\CarrierdetailskillRepositoryInterface;
+use App\Repositories\UserspecialistRepository;
+use App\Repositories\Interfaces\UserspecialistRepositoryInterface;
+use App\Repositories\UserskillRepository;
+use App\Repositories\Interfaces\UserskillRepositoryInterface;
+use App\Repositories\UserworkexperienceRepository;
+use App\Repositories\Interfaces\UserworkexperienceRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +43,20 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         $this->app->bind(CarrierGroupRepositoryInterface::class, CarrierGroupRepository::class);
+        $this->app->bind(CarrierRequirementRepositoryInterface::class, CarrierRequirementRepository::class);
+        $this->app->bind(CarrierEducationRepositoryInterface::class, CarrierEducationRepository::class);
+        $this->app->bind(CarrierIndustrialsectorRepositoryInterface::class, CarrierIndustrialsectorRepository::class);
+        $this->app->bind(CarrierSpecialistRepositoryInterface::class, CarrierSpecialistRepository::class);
+        $this->app->bind(CarrierSkillRepositoryInterface::class, CarrierSkillRepository::class);
+        $this->app->bind(CarrierRepositoryInterface::class, CarrierRepository::class);
+        $this->app->bind(CompanieRepositoryInterface::class, CompanieRepository::class);
+        $this->app->bind(CarrierdetailspecialistRepositoryInterface::class, CarrierdetailspecialistRepository::class);
+        $this->app->bind(RegencieRepositoryInterface::class, RegencieRepository::class);
+        $this->app->bind(ProvinceRepositoryInterface::class, ProvinceRepository::class);
+        $this->app->bind(CarrierdetailskillRepositoryInterface::class, CarrierdetailskillRepository::class);
+        $this->app->bind(UserspecialistRepositoryInterface::class, UserspecialistRepository::class);
+        $this->app->bind(UserskillRepositoryInterface::class, UserskillRepository::class);
+        $this->app->bind(UserworkexperienceRepositoryInterface::class, UserworkexperienceRepository::class);
         
     }
 
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\CarrierGroupController;
+use App\Http\Controllers\Api\CarrierRequirementController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +17,88 @@ Route::group(['prefix' => 'masterdata/'], function () {
         Route::post("update", [CarrierGroupController::class, "update"]);
         Route::get("show/id/{id}", [CarrierGroupController::class, "show"]);
     });
-
+    Route::group(['prefix' => 'carrierrequirements/'], function () {
+        Route::get("show/all", [CarrierRequirementController::class, "index"]);
+        Route::post("create", [CarrierRequirementController::class, "store"]);
+        Route::post("update", [CarrierRequirementController::class, "update"]);
+        Route::get("show/id/{id}", [CarrierRequirementController::class, "show"]);
+    });
+    Route::group(['prefix' => 'carriereducations/'], function () {
+        Route::get("show/all", [CarrierEducationController::class, "index"]);
+        Route::post("create", [CarrierEducationController::class, "store"]);
+        Route::post("update", [CarrierEducationController::class, "update"]);
+        Route::get("show/id/{id}", [CarrierEducationController::class, "show"]);
+    });
+    Route::group(['prefix' => 'carrierindustrialsectors/'], function () {
+        Route::get("show/all", [CarrierIndustrialsectorController::class, "index"]);
+        Route::post("create", [CarrierIndustrialsectorController::class, "store"]);
+        Route::post("update", [CarrierIndustrialsectorController::class, "update"]);
+        Route::get("show/id/{id}", [CarrierIndustrialsectorController::class, "show"]);
+    });
+    Route::group(['prefix' => 'carrierspecialists/'], function () {
+        Route::get("show/all", [CarrierSpecialistController::class, "index"]);
+        Route::post("create", [CarrierSpecialistController::class, "store"]);
+        Route::post("update", [CarrierSpecialistController::class, "update"]);
+        Route::get("show/id/{id}", [CarrierSpecialistController::class, "show"]);
+    });
+    Route::group(['prefix' => 'carrierskills/'], function () {
+        Route::get("show/all", [CarrierSkillController::class, "index"]);
+        Route::post("create", [CarrierSkillController::class, "store"]);
+        Route::post("update", [CarrierSkillController::class, "update"]);
+        Route::get("show/id/{id}", [CarrierSkillController::class, "show"]);
+    });
+    Route::group(['prefix' => 'carriers/'], function () {
+        Route::get("show/all", [CarrierController::class, "index"]);
+        Route::post("create", [CarrierController::class, "store"]);
+        Route::post("update", [CarrierController::class, "update"]);
+        Route::get("show/id/{id}", [CarrierController::class, "show"]);
+    });
+    Route::group(['prefix' => 'companies/'], function () {
+        Route::get("show/all", [CompanieController::class, "index"]);
+        Route::post("create", [CompanieController::class, "store"]);
+        Route::post("update", [CompanieController::class, "update"]);
+        Route::get("show/id/{id}", [CompanieController::class, "show"]);
+    });
+    Route::group(['prefix' => 'carrierdetailspecialists/'], function () {
+        Route::get("show/all", [CarrierdetailspecialistController::class, "index"]);
+        Route::post("create", [CarrierdetailspecialistController::class, "store"]);
+        Route::post("update", [CarrierdetailspecialistController::class, "update"]);
+        Route::get("show/id/{id}", [CarrierdetailspecialistController::class, "show"]);
+    });
+    Route::group(['prefix' => 'regencies/'], function () {
+        Route::get("show/all", [RegencieController::class, "index"]);
+        Route::post("create", [RegencieController::class, "store"]);
+        Route::post("update", [RegencieController::class, "update"]);
+        Route::get("show/id/{id}", [RegencieController::class, "show"]);
+    });
+    Route::group(['prefix' => 'provinces/'], function () {
+        Route::get("show/all", [ProvinceController::class, "index"]);
+        Route::post("create", [ProvinceController::class, "store"]);
+        Route::post("update", [ProvinceController::class, "update"]);
+        Route::get("show/id/{id}", [ProvinceController::class, "show"]);
+    });
+    Route::group(['prefix' => 'carrierdetailskills/'], function () {
+        Route::get("show/all", [CarrierdetailskillController::class, "index"]);
+        Route::post("create", [CarrierdetailskillController::class, "store"]);
+        Route::post("update", [CarrierdetailskillController::class, "update"]);
+        Route::get("show/id/{id}", [CarrierdetailskillController::class, "show"]);
+    });
+    Route::group(['prefix' => 'userspecialists/'], function () {
+        Route::get("show/all", [UserspecialistController::class, "index"]);
+        Route::post("create", [UserspecialistController::class, "store"]);
+        Route::post("update", [UserspecialistController::class, "update"]);
+        Route::get("show/id/{id}", [UserspecialistController::class, "show"]);
+    });
+    Route::group(['prefix' => 'userskills/'], function () {
+        Route::get("show/all", [UserskillController::class, "index"]);
+        Route::post("create", [UserskillController::class, "store"]);
+        Route::post("update", [UserskillController::class, "update"]);
+        Route::get("show/id/{id}", [UserskillController::class, "show"]);
+    });
+    Route::group(['prefix' => 'userworkexperiences/'], function () {
+        Route::get("show/all", [UserworkexperienceController::class, "index"]);
+        Route::post("create", [UserworkexperienceController::class, "store"]);
+        Route::post("update", [UserworkexperienceController::class, "update"]);
+        Route::get("show/id/{id}", [UserworkexperienceController::class, "show"]);
+    });
 });
